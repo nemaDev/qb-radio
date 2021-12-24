0fdd1eed87dc7b79654087e150446ab49c51b3cc
Nema <petermannfelipe@gmail.com>
2021-12-24 08:08:27 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,60 @@
+Config = {}
+
+Config.RestrictedChannels = {
+    [1] = {
+        police = true,
+        ambulance = true
+    },
+    [2] = {
+        police = true,
+        ambulance = true
+    },
+    [3] = {
+        police = true,
+        ambulance = true
+    },
+    [4] = {
+        police = true,
+        ambulance = true
+    },
+    [5] = {
+        police = true,
+        ambulance = true
+    },
+    [6] = {
+        police = true,
+        ambulance = true
+    },
+    [7] = {
+        police = true,
+        ambulance = true
+    },
+    [8] = {
+        police = true,
+        ambulance = true
+    },
+    [9] = {
+        police = true,
+        ambulance = true
+    },
+    [10] = {
+        police = true,
+        ambulance = true
+    }
+}
+
+Config.MaxFrequency = 500
+
+Config.messages = {
+    ['not_on_radio'] = 'No estás conectado a una señal',
+    ['on_radio'] = 'Ya estás conectado a esta señal.',
+    ['joined_to_radio'] = 'Estas conectado a: ',
+    ['restricted_channel_error'] = 'No puedes conectarte a esta señal!',
+    ['invalid_radio'] = 'Esta frecuencia no está disponible.',
+    ['you_on_radio'] = 'Ya estás conectado a este canal',
+    ['you_leave'] = 'Dejaste el canal.',
+    ['volume_radio'] = 'Nuevo volumen ',
+    ['decrease_radio_volume'] = 'La radio ya está configurada al volumen máximo',
+    ['increase_radio_volume'] = 'La radio ya está configurada al volumen más bajo.',
+    ['increase_decrease_radio_channel'] = 'Nuevo canal ',
+}
